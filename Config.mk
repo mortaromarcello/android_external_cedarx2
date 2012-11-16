6a7dcebdd4adceda18209ea0fe824040d2543375
mortaromarcello <mortaromarcello@gmail.com>
2012-11-16 20:48:03 +0100

    modified:   Config.mk

--- Config.mk
+++ Config.mk
@@ -31,8 +31,10 @@ TEMP_COMPILE_DISABLE := false
 endif
 
 ifeq ($(PLATFORM_VERSION),4.1.2)
+AV_BASE_PATH := av
 TEMP_COMPILE_DISABLE := true
 else
+AV_BASE_PATH := base
 TEMP_COMPILE_DISABLE := false
 endif
 
